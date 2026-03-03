259bb5122a747a3c510649602516db7762cde0af
Pham Hoang Hai <66716510+phhai-hcmut@users.noreply.github.com>
2026-03-03 21:01:43 +0700

    wrong location issue reproduction

---
+++ modules/den.nix
@@ -0,0 +1,26 @@
+{ inputs, den, ... }:
+{
+  # we can import this flakeModule even if we dont have flake-parts as input!
+  imports = [ inputs.den.flakeModule ];
+
+  den.hosts.x86_64-linux.igloo.users.tux = { };
+
+  den.aspects.igloo = {
+    nixos =
+      { pkgs, ... }:
+      {
+        environment.systemPackages = [ pkgs.hello ];
+
+        # USER TODO: remove this
+        boot.loader.grub.enable = false;
+        fileSystems."/".device = "/dev/null";
+      };
+  };
+
+  den.aspects.tux = {
+    includes = [ den.provides.primary-user ];
+    user.extraGroups = [ "audio" ];
+  };
+
+  den.default.nixos.system.stateVersion = "26.05";
+}
